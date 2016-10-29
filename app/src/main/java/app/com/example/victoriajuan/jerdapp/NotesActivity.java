50dbf41121053e8110fbc552d3acadf082c01060
VICTORIA JUAN <squintyblu@gmail.com>
2016-10-29 10:16:15 -0700

    Notes should work

--- app/src/main/java/app/com/example/victoriajuan/jerdapp/NotesActivity.java
+++ app/src/main/java/app/com/example/victoriajuan/jerdapp/NotesActivity.java
@@ -1,15 +1,28 @@
 package app.com.example.victoriajuan.jerdapp;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
+import android.text.InputType;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 /**
  * Created by victoriajuan on 10/22/16.
  */
 
 public class NotesActivity extends AppCompatActivity {
+
+    private EditText writtenNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +32,51 @@ public class NotesActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        writtenNote = (EditText) findViewById(R.id.input_notes);
+        Button saveNotesButton = (Button) findViewById(R.id.save_notes);
+        saveNotesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveNote();
+            }
+        });
     }
 
-    public boolean onOptionsItemSelected(MenuItem item){
-        this.finish();
-        return true;
-    }
+    public void saveNote() {
 
+        final String[] filename = new String[1];
+        String string = writtenNote.getText().toString();
+        FileOutputStream outputStream;
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("What project are these notes for?");
+
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                Calendar c = Calendar.getInstance();
+                SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
+                String formattedDate = df.format(c.getTime());
+
+                filename[0] = input.getText().toString() + formattedDate;
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+
+        try {
+            outputStream = openFileOutput(filename[0], Context.MODE_PRIVATE);
+            outputStream.write(string.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
 
 }
